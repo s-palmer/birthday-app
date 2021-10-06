1d4b1871737ccfcc7c78cff8bc7dda5188578a13
Sergei Palmer <zigpalmer@gmail.com>
2021-10-06 12:16:13 +0100

    Added session params and your-birthday route

--- app.rb
+++ app.rb
@@ -6,16 +6,24 @@ require 'sinatra/reloader'
 #     register Sinatra::Reloader
 #   end
 
+enable :sessions
+
 get "/" do
     erb :index
 end
 
 post "/birthday" do
-
+  session[:name] = params[:name]
+  session[:birthday_day] = params[:birthday_day]
+  session[:birthday_month] = params[:birthday_month]
   redirect to('your-birthday')
 end
 
 get "/your-birthday" do
-  
+  @name = session[:name]
+  @birthday_day = session[:birthday_day]
+  @birthday_month = session[:birthday_month]
+
+  erb :your_birthday
 end
 
